ca85d3c0d599a6039f61f5e39f2ad17bfe8f4d58
Ivan Mamaev <twixfriday@gmail.com>
2025-12-11 23:35:00 +0000

    Added format types

--- definitions/sources.js
+++ definitions/sources.js
@@ -10,11 +10,16 @@
     "tiktok_hashtags",
     "tiktok_media_urls",
     "payment_operations",
-    "promo_exp"
+    "promo_exp",
+    "promo_expenses",
+    "payment_operation",
+    "promo_releases",
+    "promo_tracks",
+    "snapshots"
 ].forEach(name =>
     declare({
-        database: "dotted-cedar-473703-a1", // your GCP project id
-        schema: "raw_tiktok", // dataset
-        name: name // BigQuery table name
+        database: "dotted-cedar-473703-a1",
+        schema: "raw_data",
+        name: name
     })
 );
